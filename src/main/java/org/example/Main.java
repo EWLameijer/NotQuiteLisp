eafa7bbcdfc5998af9ca2b5d47a9c3973eae3b54
EWLameijer <eric_wubbo@hotmail.com>
2023-03-02 14:45:32 +0100

    Less code is better code!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,15 @@ package org.example;
 class Main {
     public static void main(String[] args) throws IllegalArgumentException {
         String test = "(()(()(";
-
         int stringLength = test.length();
         int result = 0;
-        try {
-            for (int i = 0; i < stringLength; i++) {
-                char temp = test.charAt(i);
-                if (temp == '(') {
-                    result++;
-                } else if (temp == ')') {
-                    result--;
-                } else throw new IllegalArgumentException("Unknown character");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println(e);
+        for (int i = 0; i < stringLength; i++) {
+            char temp = test.charAt(i);
+            if (temp == '(') {
+                result++;
+            } else if (temp == ')') {
+                result--;
+            } else throw new IllegalArgumentException("Unknown character");
         }
         System.out.println(result);
     }
